03ac1b1f432f38a3a7b97382dceaf1f30c3aa382
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-09-22 15:32:46 -0300

    Dando significado e classes

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,9 @@ class ListaTransferencia extends StatelessWidget {
 
     return Column(
       children: [
-        ItemTransferencia('100.0', '1000'),
-        ItemTransferencia('200.0', '1000'),
-        ItemTransferencia('300.0', '2000'),
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 1000)),
+        ItemTransferencia(Transferencia(300.0, 1000)),
       ],
     );
   }
@@ -29,21 +29,27 @@ class ListaTransferencia extends StatelessWidget {
 
 class ItemTransferencia extends StatelessWidget {
 
-  final String valor;
-  final String numeroConta;
+  final Transferencia _transferencia;
 
 
-  ItemTransferencia(this.valor, this.numeroConta);
+  ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
       leading: Icon(Icons.monetization_on),
-      title: Text(valor),
-      subtitle: Text(numeroConta),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
     ));
   }
+}
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
 
 
