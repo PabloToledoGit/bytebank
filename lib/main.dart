77e4cf2dc3b40537e6d61588472763ce8e9b4664
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-09-22 14:35:25 -0300

    criando cards com o materialapps

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() =>
+    runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Transferências'),),
+          appBar: AppBar(
+            title: Text('Transferências'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+          body: ListaTransferencia(),
       ),
     ));
+
+
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100.0'),
+              subtitle: Text('1000'),
+            )),
+        Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.0'),
+              subtitle: Text('1000'),
+            )),
+      ],
+    );
+  }
+}
